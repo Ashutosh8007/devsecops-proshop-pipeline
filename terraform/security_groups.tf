6f3908530c4f765bb3eaf1f22b4639d87b0ec6a6
Ashutosh8007 <ashushirke007@gmail.com>
2026-07-31 19:36:56 +0000

    fix: add SG ingress rule for ArgoCD UI port-forward access (8080)

--- terraform/security_groups.tf
+++ terraform/security_groups.tf
@@ -119,6 +119,14 @@ resource "aws_security_group" "k3s_sg" {
   }
 
   ingress {
+    description = "Allow ArgoCD UI access via port-forward tunnel"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "Prometheus UI from my IP"
     from_port   = 9090
     to_port     = 9090
